12866a8e9e51867e65c0ef6edd4e7b97158b1960
zhiyanov <zhiyanovap@gmail.com>
2021-03-11 12:42:48 +0300

    clustermap to heatmap in shifts and fractions

--- scripts/analysis/fractions/main_isomiRs_TCGA_stability.py
+++ scripts/analysis/fractions/main_isomiRs_TCGA_stability.py
@@ -10,7 +10,7 @@ term = int(sys.argv[1])
 side = int(sys.argv[2])
 tb = pd.read_csv("../../../processed_data/TCGA/main_isomiRs.tsv", sep="\t")
 tb = tb[tb["miRNA"].str.endswith("{}p".format(side))]
-tb["miRNA"] = tb["miRNA"].str.lstrip("hsa-miR-")
+# tb["miRNA"] = tb["miRNA"].str.lstrip("hsa-miR-")
 
 tb = tb.pivot_table(
     index = "Project", columns="miRNA",
@@ -18,24 +18,34 @@ tb = tb.pivot_table(
     fill_value=0
 )
 
-for miRNA in tb.columns:
-    tb.loc[:, miRNA] = tb.loc[:, miRNA] / tb.loc[:, miRNA].sum()
+# make lexicographical order
+tb = tb.sort_index()
+tb = tb.reindex(sorted(tb.columns), axis=1)
 
-cmap = sns.color_palette("Blues")
-p = sns.clustermap(
-    tb.T, dendrogram_ratio=(.05, .2),
-    cmap=cmap, linecolor="lightgrey", linewidth=0.1,
-    cbar_kws={"label": "fraction of the main isomiR"},
+cmap = sns.color_palette("Blues", as_cmap=True)
+#p = sns.clustermap(
+#    tb.T, dendrogram_ratio=(.05, .2),
+#    cmap=cmap, linecolor="lightgrey", linewidth=0.1,
+#    cbar_kws={"label": "fraction of the main isomiR"},
+#)
+
+#plt.setp(p.ax_heatmap.get_xticklabels(), rotation=45, ha="right")
+#plt.setp(p.ax_heatmap.get_yticklabels(), rotation=0)
+#p.ax_heatmap.set_xlabel("")
+#p.ax_heatmap.set_ylabel("")
+
+fig, ax = plt.subplots(figsize=(8,8))
+ax = sns.heatmap(
+    tb.T,
+    cmap=cmap, linecolor="lightgrey", linewidth=0.05,
+    cbar_kws={"label": "Fraction of the main isomiR"},
+    ax=ax
 )
 
-plt.setp(p.ax_heatmap.get_xticklabels(), rotation=45, ha="right")
-plt.setp(p.ax_heatmap.get_yticklabels(), rotation=0)
-p.ax_heatmap.set_xlabel("")
-p.ax_heatmap.set_ylabel("")
-# p.ax_heatmap.set_yticklabels("")
-# plt.title("{}term_{}p.png".format(term, side), loc="left", fontdict={"fontsize": "xx-large", "fontweight": "bold"})
-
+ax.set_xlabel("")
+ax.set_ylabel("")
 plt.tight_layout()
+
 plt.savefig(
     OUTPUT_DIR + "{}term_{}p.png".format(term, side)
 )
--- scripts/analysis/shifts/main_isomiRs_TCGA_stability.py
+++ scripts/analysis/shifts/main_isomiRs_TCGA_stability.py
@@ -10,7 +10,7 @@ term = int(sys.argv[1])
 side = int(sys.argv[2])
 tb = pd.read_csv("../../../processed_data/TCGA/main_isomiRs.tsv", sep="\t")
 tb = tb[tb["miRNA"].str.endswith("{}p".format(side))]
-tb["miRNA"] = tb["miRNA"].str.lstrip("hsa-miR-")
+# tb["miRNA"] = tb["miRNA"].str.lstrip("hsa-miR-")
 
 tb = tb.pivot_table(
         index = "shift_{}".format(term),
@@ -19,25 +19,57 @@ tb = tb.pivot_table(
         fill_value=0
 )
 
+shifts = list(range(-2, 3))
+# adding missed shifts
+for shift in shifts:
+    if shift not in list(tb.index):
+        tb.loc[shift] = 0
+
 for miRNA in tb.columns:
     tb.loc[:, miRNA] = tb.loc[:, miRNA] / tb.loc[:, miRNA].sum()
 
-cmap = sns.color_palette("Blues")
-p = sns.clustermap(
-    tb.T, dendrogram_ratio=(.05, .2),
-    cmap=cmap, linecolor="lightgrey", linewidth=0.1,
-    cbar_kws={"label": "fraction of TCGA projects"},
-    col_cluster=False
+# drop too large shifts
+tb = tb.drop(list(set(tb.index) - set(shifts)))
+
+# make lexicographical order
+tb = tb.sort_index()
+tb = tb.reindex(sorted(tb.columns), axis=1)
+
+
+cmap = sns.color_palette("Blues", as_cmap=True)
+#p = sns.clustermap(
+#    tb.T, dendrogram_ratio=(.05, .2), figsize=(7.5, 8),
+#    cmap=cmap, linecolor="lightgrey", linewidth=0.05,
+#    cbar_kws={"label": "fraction of TCGA projects"},
+#    col_cluster=False
+#)
+
+#plt.setp(p.ax_heatmap.get_xticklabels(), rotation=0)
+#plt.setp(p.ax_heatmap.get_yticklabels(), rotation=0)
+#p.ax_row_dendrogram.set_visible(False)
+#p.ax_heatmap.set_xlabel("")
+#p.ax_heatmap.set_ylabel("")
+
+#plt.tight_layout()
+#plt.savefig(
+#    OUTPUT_DIR + "{}term_{}p.png".format(term, side)
+#)
+
+fig, ax = plt.subplots(figsize=(8,8))
+ax = sns.heatmap(
+    tb.T,
+    cmap=cmap, linecolor="lightgrey", linewidth=0.05,
+    cbar_kws={"label": "Fraction of TCGA projects"},
+    ax=ax
 )
 
-plt.setp(p.ax_heatmap.get_xticklabels(), rotation=45, ha="right")
-plt.setp(p.ax_heatmap.get_yticklabels(), rotation=0)
-p.ax_heatmap.set_xlabel("")
-p.ax_heatmap.set_ylabel("")
-# p.ax_heatmap.set_yticklabels("")
-# plt.title("{}term_{}p.png".format(term, side), loc="left", fontdict={"fontsize": "xx-large", "fontweight": "bold"})
-
+ax.set_xlabel("")
+ax.set_ylabel("")
 plt.tight_layout()
 plt.savefig(
     OUTPUT_DIR + "{}term_{}p.png".format(term, side)
 )
+
+
+
+
